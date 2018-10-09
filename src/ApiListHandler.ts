e277a490cbdb77b81b01b728e300c24cde8cdc62
Andrew Smithson <smithson@uk.ibm.com>
2018-10-09 09:43:28 +0100

    :children_crossing: Display more API Information
    Add the version and description of an API to the list of APIs displayed

--- src/ApiListHandler.ts
+++ src/ApiListHandler.ts
@@ -1,14 +1,19 @@
 import {ICommandHandler, IHandlerParameters} from "@brightside/imperative"
 import {ZosConnect} from "./ZosConnect"
+import {Api} from "./Api"
 
 export default class ApiListHandler implements ICommandHandler {
     public async process(commandParameters: IHandlerParameters) {
         let zosConn = new ZosConnect({uri: commandParameters.profiles.get("zosconnect").address});
         try {
             let apis = await zosConn.getApis();
+            let resultsObj = [];
+            for(const api of apis){
+                resultsObj.push({name: api.getApiName(), version: api.getVersion(), description: api.getDescription()});
+                commandParameters.response.console.log(`${api.getApiName()}(${api.getVersion()}) - ${api.getDescription()}`);
+            }
             
-            commandParameters.response.data.setObj(apis);
-            commandParameters.response.console.log(apis.join('\n'));
+            commandParameters.response.data.setObj(resultsObj);
         } catch (error) {
             commandParameters.response.console.error(error);
         }
