5432484f69dbd90a5fee56619fd3a51540e53b3f
Andrew Smithson <smithson@uk.ibm.com>
2018-11-29 17:54:41 +0000

    Better error handling for service list command

--- src/cli/service/list/list.handler.ts
+++ src/cli/service/list/list.handler.ts
@@ -1,5 +1,6 @@
-import {ICommandHandler, IHandlerParameters} from "@brightside/imperative";
-import {ConnectionUtil} from "../../../connection";
+import { ICommandHandler, IHandlerParameters } from "@brightside/imperative";
+import { ConnectionUtil } from "../../../connection";
+import { StatusCodeError } from "request-promise/errors";
 
 export default class ServiceListHandler implements ICommandHandler {
     public async process(commandParameters: IHandlerParameters): Promise<void> {
@@ -12,7 +13,21 @@ export default class ServiceListHandler implements ICommandHandler {
                 commandParameters.response.console.log(`${service.getName()} - ${service.getDescription()} [${service.getServiceProvider()}]`);
             }
         } catch (error) {
-            commandParameters.response.console.error(error);
+            switch(error.constructor){
+                case StatusCodeError:
+                    let statusCodeError = error as StatusCodeError;
+                    switch(statusCodeError.statusCode){
+                        case 401:
+                        case 403:
+                            commandParameters.response.console.error('Security error, unable to display Services');
+                            break;
+                        default:
+                            commandParameters.response.console.error(statusCodeError.message);
+                    }
+                    break;
+                default:
+                    commandParameters.response.console.error(error);
+            }
         }
     }
 }
