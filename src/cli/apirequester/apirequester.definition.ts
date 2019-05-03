6d99497a82f3d26b929820e7a5eac7b1697f866b
Andrew Smithson <smithson@uk.ibm.com>
2019-05-03 11:27:51 +0100

    API Request profile overrides
    Signed-off-by: Andrew Smithson <smithson@uk.ibm.com>

--- src/cli/apirequester/apirequester.definition.ts
+++ src/cli/apirequester/apirequester.definition.ts
@@ -1,4 +1,5 @@
 import { ICommandDefinition } from "@brightside/imperative";
+import { ZosConnectSessionutils } from "../ZosConnectionSessionUtils";
 import { DeleteDefinition } from "./delete/delete.definition";
 import { InstallDefinition } from "./install/install.definition";
 import { ListDefinition } from "./list/list.definition";
@@ -11,6 +12,16 @@ const apiRequsterDefinition: ICommandDefinition = {
     type: "group",
     description: "Manage z/OS Connect EE API Requesters",
     children: [ListDefinition, InstallDefinition, DeleteDefinition, UpdateDefinition, StartDefinition, StopDefinition],
+    passOn: [
+        {
+            property: "options",
+            value: ZosConnectSessionutils.ZCON_CONNECTION_OPTIONS,
+            merge: true,
+            ignoreNodes: [
+                {type: "group"},
+            ],
+        },
+    ],
 };
 
 export = apiRequsterDefinition;
