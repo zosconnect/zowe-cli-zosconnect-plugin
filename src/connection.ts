eca0a17593fd613006763f1ea7eafd77ccdf7148
Andrew Smithson <smithson@uk.ibm.com>
2019-05-24 11:36:51 +0100

    Correct adding new parameter and code tidyup
    Signed-off-by: Andrew Smithson <smithson@uk.ibm.com>

--- src/connection.ts
+++ src/connection.ts
@@ -9,16 +9,11 @@
  *
  */
 
-import { IProfile, Session } from "@brightside/imperative";
 import { ZosConnect } from "@zosconnect/zosconnect-node";
 import request = require("request");
 import { ZosConnectSession } from "./ZosConnectSession";
 
 export class ConnectionUtil {
-    public static getSession(profile: IProfile): ZosConnectSession {
-        return new ZosConnectSession(profile.address, profile.user, profile.password);
-    }
-
     public static getConnection(session: ZosConnectSession): ZosConnect {
         const options = {} as request.OptionsWithUri;
         options.uri = session.address;
